cfc676bb2ace43909f69e137e96291645cf9883a
Roberto de León <49464343+DiracSpace@users.noreply.github.com>
2021-04-23 22:50:30 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -16,16 +16,4 @@ path  = astar.search
 
 puts graph.to_s(path)
 
-# puts "\n"
-
-# dungeon2 = File.open("laberinto2.txt", "r").read
-
-# graph2 = Graph.new(dungeon2)
-
-# astar2 = ASTAR.new(graph2.start, graph2.stop)
-
-# path2  = astar2.search
-
-# puts graph.to_s(path2)
-
 exit 0
